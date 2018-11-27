b3516af617a8a4a2ee116fc4d3afacb3b6995d0d
avpolonn <plurim@mail.ru>
2018-11-27 18:58:40 +0300

    les8

--- code/les8.py
+++ code/les8.py
@@ -4,3 +4,16 @@ import time
 
 driver = webdriver.Chrome()
 driver.get("http://localhost:8080/litecart/")
+pr=driver.find_element_by_id("popular-products")
+fpr=pr.find_elements_by_class_name("product")
+count = 0
+t = "тайтл есть"
+for kek in fpr:
+    for stc in (kek.find_elements_by_class_name("sticker")):
+        count+=1
+        if len(stc.text)<0:
+            t="тайтла где-то нет"
+    print(count,"стикер,", t)
+    count=0
+
+driver.quit()
