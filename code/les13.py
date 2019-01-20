eec8bac7e53b5fb90acfb340286b21265678f74e
avpolonn <plurim@mail.ru>
2019-01-20 23:05:51 +0300

    Update les13.py

--- code/les13.py
+++ code/les13.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import time
 
 driver = webdriver.Chrome()
 driver.implicitly_wait(2)
@@ -15,22 +15,31 @@ k=0
 while k<3:
     value = int(driver.find_element_by_css_selector("span.quantity").get_attribute("textContent"))
     newv = str(value + 1)
-    driver.find_element_by_css_selector("div#box-campaign-products a").click()
+    driver.find_element_by_css_selector("li.product").click()
     driver.find_element_by_css_selector('button[name="add_cart_product"]').click()
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//span[@class='quantity'][text()='" + newv + "']")))
-    driver.find_element_by_css_selector('div[aria-label="Close"]').click()
+    driver.get("http://localhost:8080/litecart/")
     k=k+1
 
 driver.find_element_by_css_selector("#cart").click()
+allprod=driver.find_elements_by_css_selector("ul.items>li")
+lenprod = len(allprod)
+cprod=1
 
-count_prod=int(driver.find_element_by_css_selector("tr.item>td:nth-child(4) input").get_attribute("value"))
-while count_prod>0:
-    count_prod=count_prod-1
-    driver.find_element_by_css_selector("tr.item>td:nth-child(4) input").clear()
-    summ=driver.find_element_by_css_selector("#box-checkout-summary>table>tbody>tr:nth-child(1)>td:nth-child(2)").get_attribute("textContent")
-    driver.find_element_by_css_selector("tr.item>td:nth-child(4) input").send_keys(count_prod)
-    driver.find_element_by_css_selector("i.fa.fa-refresh").click()
-    WebDriverWait(driver, 10).until_not(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#box-checkout-summary>table>tbody>tr:nth-child(1)>td:nth-child(2)"), summ))
+while lenprod > 0:
+    priceone=driver.find_element_by_css_selector("tr.footer>td:nth-child(2)").get_attribute("textContent")
+    d = allprod[0].find_element_by_css_selector("p:nth-child(4)")
+    d.click()
+    # если позиций больше 1, то ждем пока изменится цена
+    if lenprod>1:
+        WebDriverWait(driver, 10).until_not(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "tr.footer>td:nth-child(2)"), priceone))
+    # если позиция одна, ждем до появления текста
+    else:
+        WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element((By.CSS_SELECTOR, "#checkout-cart-wrapper p:nth-child(1)"),"There are no items in your cart."))
+    allprod = driver.find_elements_by_css_selector("ul.items>li")
+    lenprod = len(allprod)
+
+
 
 
 driver.quit()
